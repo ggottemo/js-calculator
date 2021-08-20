0551317c7193903f31b4189e0c8a537dae22701a
ggottemo <ggottemo717@gmail.com>
2021-08-20 00:20:52 -0400

    reformat and remove old comments

--- calculator.js
+++ calculator.js
@@ -24,36 +24,36 @@ function mod(x, y) {
 var isOp = true;
 //Create a calculator object to store numbers and operator
 const calculator = {
-    firstNum : '',
+    firstNum: '',
     secondNum: '',
     //Store both operator and symbol for operation for easier 
     //parsing of second num
-    operator : null,
+    operator: null,
     operatorSymbol: null,
-    setFirst : function(x) {
-        if(x.indexOf('.') != -1) //If String has decimal, number is a float
+    setFirst: function (x) {
+        if (x.indexOf('.') != -1) //If String has decimal, number is a float
             this.firstNum = parseFloat(x);
         else this.firstNum = parseInt(x);
         console.log('First Num: ' + this.firstNum);
     },
-    setSecond: function() {
+    setSecond: function () {
         let display = document.getElementById('display');
         let index = display.innerText.indexOf(calculator.operatorSymbol);
         console.log('Index:' + index);
-        let secondHalf = display.innerText.substring(index+2)
+        let secondHalf = display.innerText.substring(index + 2)
         if (secondHalf.indexOf('.') != -1) //Checking if second half is float.
             this.secondNum = parseFloat(secondHalf);
-        else 
-        this.secondNum = parseInt(secondHalf);
+        else
+            this.secondNum = parseInt(secondHalf);
     },
-    setOperator : function(x) {
+    setOperator: function (x) {
         this.operator = x;
     },
-    setOperatorSymbol: function(x) {
+    setOperatorSymbol: function (x) {
         this.operatorSymbol = x;
     },
     // Clear stored info
-    reset : function() {
+    reset: function () {
         this.firstNum = '';
         this.secondNum = '';
         this.operator = null;
@@ -68,7 +68,6 @@ function operate(operator, x, y) {
     window[operator](x, y);
 }
 //display(int x) -> Updates display with number or operator.
-//TODO: Check if already a number, and edit instead
 function display(x) {
     let display = document.getElementById('display');
     //Check if an operator has been chosen to add space
@@ -82,7 +81,7 @@ function display(x) {
     }
 }
 
-function clearDisplay(){
+function clearDisplay() {
     let display = document.getElementById('display');
     display.innerHTML = "";
     calculator.reset();
@@ -122,15 +121,12 @@ function addListeners() {
                 case 'item-12':         // SUBTRACT
                     console.log('-');
                     calculator.setOperator("subtract");
-                    calculator.setOperatorSymbol('-');
+                    calculator.setOperatorSymbol('-'); 
                     break;
-                    case 'item-16':         // ADD
+                case 'item-16':         // ADD
                     calculator.setOperator("add")
                     calculator.setOperatorSymbol('+');
                     break;
-                // case 'item-18':         // DECIMAL
-                //     console.log('.');
-                //     break;
                 case 'item-19':         // OPERATE
                     if (calculator.operator != null) {
                         calculator.setSecond();
@@ -138,7 +134,7 @@ function addListeners() {
                     }
                     console.log('=');
                     break;
-            
+
                 default:
                     isOp = false;
                     display(e.target.innerText);
